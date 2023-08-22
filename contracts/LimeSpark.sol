d75bddcb4c0b2f78d41d6f321954d566c81e7e25
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-08-22 17:03:41 +0300

    refactor: change LimeSpark token

--- contracts/LimeSpark.sol
+++ contracts/LimeSpark.sol
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract LimeSpark is ERC20, ERC20Burnable, Ownable {
+contract LimeSpark is ERC20, Ownable {
     error TokensAlreadyMinted();
 
     uint256 public immutable starterTokens;
