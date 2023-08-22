f0cef6f8ad326bcd7a1bb3f482970e151b8ce4ab
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-08-22 16:46:31 +0300

    feat: add inital minting functionality

--- contracts/LimeSpark.sol
+++ contracts/LimeSpark.sol
@@ -6,9 +6,30 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract LimeSpark is ERC20, ERC20Burnable, Ownable {
-    constructor() ERC20("LimeSpark", "LSK") {}
+    error TokensAlreadyMinted();
 
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
+    uint256 public immutable starterTokens;
+
+    constructor(uint256 _starterTokens) ERC20("LimeSpark", "LSK") {
+        starterTokens = _starterTokens;
+    }
+
+    mapping(address => bool) accountHaveMinted;
+
+    modifier starterTokensNotMinted(address account) {
+        if (accountHaveMinted[account]) {
+            revert TokensAlreadyMinted();
+        }
+
+        _;
+    }
+
+    function mintInitial() external starterTokensNotMinted(msg.sender) {
+        accountHaveMinted[msg.sender] = true;
+        _mint(msg.sender, starterTokens);
+    }
+
+    function mint(address _to, uint256 _amount) public onlyOwner {
+        _mint(_to, _amount);
     }
 }
--- test/LimeSpark.ts
+++ test/LimeSpark.ts
@@ -6,7 +6,7 @@ import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 describe("LimeSpark", function () {
   async function deployFixture() {
     const LimeSpark = await ethers.getContractFactory("LimeSpark");
-    const limeSpark = await LimeSpark.deploy();
+    const limeSpark = await LimeSpark.deploy(ethers.parseUnits("100", 18));
 
     const [owner, otherAccount] = await ethers.getSigners();
 
@@ -14,21 +14,22 @@ describe("LimeSpark", function () {
   }
 
   describe("Deployment", () => {
-    it("Should deploy with correct name, symbol and supply", async () => {
+    it("Should deploy with correct name, symbol and starterTokens", async () => {
       const { limeSpark } = await loadFixture(deployFixture);
 
-      const _name = await limeSpark.name();
-      const _symbol = await limeSpark.symbol();
+      const name = await limeSpark.name();
+      const symbol = await limeSpark.symbol();
+      const starterTokens = await limeSpark.starterTokens();
 
-      expect(_name).to.be.equal("LimeSpark");
-      expect(_symbol).to.be.equal("LSK");
+      expect(name).to.be.equal("LimeSpark");
+      expect(symbol).to.be.equal("LSK");
+      expect(starterTokens).to.be.equal(100000000000000000000n);
     });
   });
 
   describe("Decimals", () => {
     it("Should return the correct decimals for user representation", async () => {
       const { limeSpark } = await loadFixture(deployFixture);
-
       expect(await limeSpark.decimals()).to.be.equal(18);
     });
   });
@@ -43,13 +44,40 @@ describe("LimeSpark", function () {
         expect(await limeSpark.balanceOf(otherAccount)).to.be.equal(10);
       });
     });
+
     describe("Validation", () => {
-      it("Should revert if not owneer tries to mint tokens", async () => {
+      it("Should revert if not owner tries to mint tokens", async () => {
         const { limeSpark, otherAccount } = await loadFixture(deployFixture);
 
         await expect(
           limeSpark.connect(otherAccount).mint(otherAccount, 10),
         ).to.revertedWith("Ownable: caller is not the owner");
+      });
+    });
+  });
+
+  describe("Mint Initial", () => {
+    describe("Action", () => {
+      it("Should mint starter tokens", async () => {
+        const { limeSpark, owner } = await loadFixture(deployFixture);
+
+        await limeSpark.mintInitial();
+        expect(await limeSpark.balanceOf(owner)).to.be.equal(
+          ethers.parseUnits("100", 18),
+        );
+      });
+    });
+
+    describe("Validation", () => {
+      it("Should revert if user tries to mint initial tokens more than once", async () => {
+        const { limeSpark, otherAccount } = await loadFixture(deployFixture);
+
+        await limeSpark.mintInitial();
+
+        await expect(limeSpark.mintInitial()).to.revertedWithCustomError(
+          limeSpark,
+          "TokensAlreadyMinted",
+        );
       });
     });
   });
