1ae410a1916950dc2b086a1184204edb47ffb2a1
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-08-24 14:08:18 +0300

    feat: remove Stakenet no longer inherits Ownable

--- contracts/Stakenet.sol
+++ contracts/Stakenet.sol
@@ -3,14 +3,13 @@ pragma solidity 0.8.19;
 
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { ERC20Burnable } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
 import { Math } from "@openzeppelin/contracts/utils/math/Math.sol";
 import { SafeMath } from "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 import { LimeSpark } from "./LimeSpark.sol";
 
-contract Stakenet is ERC20, ERC20Burnable, Ownable {
+contract Stakenet is ERC20, ERC20Burnable {
     error AccountHasAlreadyStaked();
     error AccountHasNotStaked();
     error TokensNotUnlockedYet(uint256 unlockTime);
