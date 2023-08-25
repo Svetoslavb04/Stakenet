6e048738e674be14b861e591aa95669962cce51e
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-08-25 14:31:22 +0300

    feat: add documentation for Stakenet

--- contracts/Stakenet.sol
+++ contracts/Stakenet.sol
@@ -7,29 +7,59 @@ import { ERC20Burnable } from "@openzeppelin/contracts/token/ERC20/extensions/ER
 import { Math } from "@openzeppelin/contracts/utils/math/Math.sol";
 import { SafeMath } from "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
+/// @title Stakenet Smart Contract
+/// @dev The contract that allows staking and yield farming.
+/// @dev The staking position is represented an ERC20 token
 contract Stakenet is ERC20, ERC20Burnable {
+    /// @dev Error indicating that an account has already staked tokens. Account can stake only once.
     error AccountHasAlreadyStaked();
+
+    /// @dev Error indicating that an account has not staked tokens.
     error AccountHasNotStaked();
+
+    /// @dev Error indicating that staked tokens are not yet unlocked.
     error TokensNotUnlockedYet(uint256 unlockTime);
+
+    /// @dev Error indicating an invalid stake limit configuration.
     error InvalidStakeLimit();
+
+    /// @dev Error indicating that the specified yield percentage is too large.
     error YieldPercentageTooBig(uint256 yield);
+
+    /// @dev Error indicating that a staking amount is above the limits.
     error StakeTooHigh(uint256 userStakeLimit, uint256 contractStakeLimit);
+
+    /// @dev Error indicating that a staking amount is too low and the user will not receive a reward.
     error StakeTooLow(uint256 minimumStake);
 
+    /// @dev The ERC20 token contract used for staking.
     ERC20 public erc20;
 
+    /// @dev The duration for which staked tokens are locked.
     uint256 public immutable lockDurationInSeconds;
+
+    /// @dev The yield percentage for yield farming represented with 4 decimals.
     uint24 public immutable yieldPercentage;
 
+    /// @dev The total rewards available for yield farming.
     uint256 public rewards;
 
+    /// @dev The maximum number of tokens the contract can hold for staking.
     uint256 public contractStakeLimit;
+
+    /// @dev The maximum number of tokens a user can stake.
     uint256 public userStakeLimit;
+
+    /// @dev The minimum staking amount for a user based on yield percentage.
     uint256 public userMinimumStake;
 
+    /// @dev Mapping to track whether an account has staked tokens.
     mapping(address => bool) public userHasStaked;
+
+    /// @dev Mapping to store the staked timestamp for each user.
     mapping(address => uint256) public userStakedTimestamp;
 
+    /// @dev Modifier to ensure that an account has not staked tokens.
     modifier hasNotStaked() {
         if (userHasStaked[msg.sender]) {
             revert AccountHasAlreadyStaked();
@@ -38,6 +68,7 @@ contract Stakenet is ERC20, ERC20Burnable {
         _;
     }
 
+    /// @dev Modifier to ensure that an account has staked tokens.
     modifier hasStaked() {
         if (!userHasStaked[msg.sender]) {
             revert AccountHasNotStaked();
@@ -46,6 +77,13 @@ contract Stakenet is ERC20, ERC20Burnable {
         _;
     }
 
+    /// @dev Contract constructor.
+    /// @param erc20TokenAddress The address of the ERC20 token used for staking.
+    /// @param _lockDurationInSeconds The duration for which staked tokens are locked.
+    /// @param _rewards The total rewards available for yield farming.
+    /// @param _contractStakeLimit The maximum number of tokens the contract can hold for staking.
+    /// @param _userStakeLimit The maximum number of tokens a user can stake.
+    /// @notice The yield percentage is calculated based on the rewards and the contractStakeLimit
     constructor(
         address erc20TokenAddress,
         uint256 _lockDurationInSeconds,
@@ -58,6 +96,7 @@ contract Stakenet is ERC20, ERC20Burnable {
         lockDurationInSeconds = _lockDurationInSeconds;
         rewards = _rewards;
 
+        // Check and set stake limits
         if (
             _contractStakeLimit == 0 ||
             _userStakeLimit == 0 ||
@@ -74,6 +113,8 @@ contract Stakenet is ERC20, ERC20Burnable {
         userMinimumStake = calculateMinStake();
     }
 
+    /// @dev Stake a specified amount of tokens to start yield farming.
+    /// @param _amount The amount of tokens to stake.
     function stake(uint256 _amount) external hasNotStaked {
         if (contractStakeLimit < _amount || userStakeLimit < _amount) {
             revert StakeTooHigh(userStakeLimit, contractStakeLimit);
@@ -90,6 +131,8 @@ contract Stakenet is ERC20, ERC20Burnable {
         erc20.transferFrom(msg.sender, address(this), _amount);
     }
 
+    /// @dev Transfer staking position to another address.
+    /// @param _to The address to which the staking position will be transferred.
     function transferPosition(address _to) external hasStaked {
         userStakedTimestamp[_to] = Math.max(
             userStakedTimestamp[_to],
@@ -99,6 +142,7 @@ contract Stakenet is ERC20, ERC20Burnable {
         _transfer(msg.sender, _to, balanceOf(msg.sender));
     }
 
+    /// @dev Withdraw staked tokens along with accumulated yield after the lock duration.
     function withdraw() external hasStaked {
         if (
             block.timestamp <=
@@ -123,10 +167,13 @@ contract Stakenet is ERC20, ERC20Burnable {
         erc20.transfer(msg.sender, stakedTokens + accumulatedYield);
     }
 
+    /// @dev Get the number of decimal places for the yield percentage.
+    /// @return The number of decimal places.
     function yieldDecimals() external pure returns (uint8) {
         return 4;
     }
 
+    /// @dev Calculate the yield percentage based on rewards and stake limits.
     function calculateYield() internal view returns (uint24) {
         uint256 yield = (rewards * 100_0000) / contractStakeLimit;
 
@@ -137,14 +184,19 @@ contract Stakenet is ERC20, ERC20Burnable {
         return uint24(yield);
     }
 
+    /// @dev Calculate the contract's stake limit based on rewards and yield percentage.
     function calculateContractStakeLimit() internal view returns (uint256) {
         return (rewards * 100_0000) / yieldPercentage;
     }
 
+    /// @dev Calculate the minimum stake allowed based on yield percentage.
     function calculateMinStake() public view returns (uint256) {
         return 100_0000 / yieldPercentage;
     }
 
+    /// @dev Calculate the accumulated yield for a specified number of tokens.
+    /// @param tokens The number of tokens to calculate yield for.
+    /// @return accumulatedYield The accumulated yield.
     function calculateAccumulatedYield(
         uint256 tokens
     ) internal view returns (uint256 accumulatedYield) {
