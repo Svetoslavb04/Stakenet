ba00004102ae88475fe74935e569abc6dd63fe0b
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-09-07 18:10:48 +0300

    feat: add documentation for StakenetTestERC20 Contract

--- contracts/StakenetTestERC20.sol
+++ contracts/StakenetTestERC20.sol
@@ -6,10 +6,16 @@ import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 /// @title StakenetTestERC20 Token TEST Contract
 /// @dev An ERC20 token contract that acts maliciously and is used for testing.
 /// @notice Do not use this contract in production, it is only for TEST purposes
+/// This contract extends the ERC20 standard and introduces some malicious behavior
+/// for testing purposes. It should not be used in any production environment.
 contract StakenetTestERC20 is ERC20 {
     /// @dev Initializes the StakenetTestERC20 contract.
     constructor() ERC20("StakenetTestERC20", "STE") {}
 
+    /// @dev Overrides the transfer function to avoid reverts.
+    /// @param to The recipient address.
+    /// @param amount The amount to transfer.
+    /// @return Whether the transfer was successful or not.
     function transfer(
         address to,
         uint256 amount
@@ -23,6 +29,11 @@ contract StakenetTestERC20 is ERC20 {
         return success;
     }
 
+    /// @dev Overrides the transferFrom function to avoid reverts.
+    /// @param from The sender's address.
+    /// @param to The recipient address.
+    /// @param amount The amount to transfer.
+    /// @return Whether the transfer was successful or not.
     function transferFrom(
         address from,
         address to,
@@ -45,10 +56,17 @@ contract StakenetTestERC20 is ERC20 {
         return true;
     }
 
+    /// @dev Allows external callers to mint new tokens.
+    /// @param amount The amount of tokens to mint.
     function mint(uint256 amount) external {
         _mint(msg.sender, amount);
     }
 
+    ///@dev Internal function for transferring tokens.
+    /// @param from The sender's address.
+    /// @param to The recipient's address.
+    /// @param amount The amount of tokens to transfer.
+    /// @return Whether the transfer was successful or not.
     function _transferTokens(
         address from,
         address to,
@@ -68,6 +86,11 @@ contract StakenetTestERC20 is ERC20 {
         return true;
     }
 
+    /// @dev Internal function for spending token allowance.
+    /// @param owner The owner of the tokens.
+    /// @param spender The spender of the tokens.
+    /// @param amount The amount of tokens to spend.
+    /// @return Whether the token allowance was spent successfully or not.
     function _spendTokenAllowance(
         address owner,
         address spender,
